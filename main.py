98a02bc0a7d1a8bfdf38b1164b66197a7c9496e6
Suraj Yadav <qwertypad7612@gmail.com>
2024-06-12 16:31:28 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from RedWineQuality import logger
+from RedWineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("My name is Suraj!")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
