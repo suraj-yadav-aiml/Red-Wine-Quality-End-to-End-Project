84fc7674b7ac3d2b9c230bb235831c3fc37b7659
Suraj Yadav <qwertypad7612@gmail.com>
2024-06-13 01:32:10 +0530

    Model Evaluation added

--- src/RedWineQuality/config/configuration.py
+++ src/RedWineQuality/config/configuration.py
@@ -4,6 +4,7 @@ from RedWineQuality.entity.config_entity import DataIngestionConfig
 from RedWineQuality.entity.config_entity import DataValidationConfig
 from RedWineQuality.entity.config_entity import DataTransformationConfig
 from RedWineQuality.entity.config_entity import ModelTrainerConfig
+from RedWineQuality.entity.config_entity import ModelEvaluationConfig
 
 
 class ConfigurationManager:
@@ -79,3 +80,22 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name
+           
+        )
+
+        return model_evaluation_config
